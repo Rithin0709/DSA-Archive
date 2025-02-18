6570769beca801dbd203c70d73393c14c76bc9b6
RITHIN R S <149788609+Rithin0709@users.noreply.github.com>
2025-02-18 14:53:27 +0530

    Update Selection Sort.c

--- Array/Selection Sort.c
+++ Array/Selection Sort.c
@@ -3,7 +3,10 @@
 
 void SelectionSort(int *arr,int size)
 {
-  for(int 
+  for(int i=0; i<size; i++)
+    {
+      int min = i;
+      
 }
 int main()
 {  
@@ -11,6 +14,12 @@ int main()
   printf("Enter the Size of The array\n");
   scanf("%d",&size);
   int *arr = (int *) malloc(size * sizeof(int));
+
+   if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1; 
+    }
+  
   for(int i=0; i<size; i++)
     {
       scanf("%d",&arr[i]);
