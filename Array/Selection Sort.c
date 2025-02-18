46b9512651b88077dc492f2a44a2bf708664e494
RITHIN R S <149788609+Rithin0709@users.noreply.github.com>
2025-02-18 14:51:43 +0530

    Update Selection Sort.c

--- Array/Selection Sort.c
+++ Array/Selection Sort.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+void SelectionSort(int *arr,int size)
+{
+  for(int 
+}
+int main()
+{  
+  int size;
+  printf("Enter the Size of The array\n");
+  scanf("%d",&size);
+  int *arr = (int *) malloc(size * sizeof(int));
+  for(int i=0; i<size; i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+  SelectionSort(arr,size);
+}
